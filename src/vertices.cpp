0bac8a8ae58c06cb50685fe324a0997ead61bd2f
ItamarAharony <itamar.aharony.ia@gmail.com>
2025-01-08 12:41:04 -0500

    updated vertices

--- src/vertices.cpp
+++ src/vertices.cpp
@@ -19,12 +19,12 @@ const float vertices[] = {
     0.5f, -0.5f, -0.5f,  0.0f, 0.0f,  // Bottom-left
 
     // Left face (adjust texture coordinates)
-    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,  // Top-right
-    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,  // Bottom-right
-    -0.5f, -0.5f, -0.5f,  1.0f, 0.0f,  // Bottom-left
-    -0.5f, -0.5f, -0.5f,  1.0f, 0.0f,  // Bottom-left
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,  // Top-left
-    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,  // Top-right
+    -0.5f,  0.5f,  0.5f,  1.0f, 1.0f,  // Top-right
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,  // Bottom-right
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,  // Bottom-left
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,  // Bottom-left
+    -0.5f, -0.5f,  0.5f,  1.0f, 0.0f,  // Top-left
+    -0.5f,  0.5f,  0.5f,  1.0f, 1.0f,  // Top-right
 
     // Right face (adjust texture coordinates)
      0.5f,  0.5f,  0.5f,  0.0f, 1.0f,  // Top-right
