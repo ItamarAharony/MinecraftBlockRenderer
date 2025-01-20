f1c5a0c493e2a0a850dd647803506d0d3b11f87f
ItamarAharony <itamar.aharony.ia@gmail.com>
2025-01-20 14:39:44 -0500

    updated demo gif in assets/

--- src/main.cpp
+++ src/main.cpp
@@ -104,7 +104,7 @@ int main() {
         processInput(window);
 
         // Render
-        glClearColor(0.2f, 0.8f, 0.8f, 1.0f);
+        glClearColor(0.1f, 0.5f, 0.5f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Use the shader program
